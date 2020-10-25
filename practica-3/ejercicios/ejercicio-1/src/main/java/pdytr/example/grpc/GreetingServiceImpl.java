29e4d2f3c9dca0a4d84047826923253d02872558
Andrés Milla <andressmilla@gmail.com>
2020-10-24 21:18:33 -0300

    Added experiment for client

--- practica-3/ejercicios/ejercicio-1/src/main/java/pdytr/example/grpc/GreetingServiceImpl.java
+++ practica-3/ejercicios/ejercicio-1/src/main/java/pdytr/example/grpc/GreetingServiceImpl.java
@@ -3,21 +3,23 @@ package pdytr.example.grpc;
 import io.grpc.stub.StreamObserver;
 
 public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
-  @Override
-  public void greeting(GreetingServiceOuterClass.HelloRequest request,
-        StreamObserver<GreetingServiceOuterClass.HelloResponse> responseObserver) {
-    // HelloRequest has toString auto-generated.
-    System.out.println(request);
+    @Override
+    public void greeting(GreetingServiceOuterClass.HelloRequest request,
+                         StreamObserver<GreetingServiceOuterClass.HelloResponse> responseObserver) {
+        // HelloRequest has toString auto-generated.
+        System.out.println(request);
 
-    // You must use a builder to construct a new Protobuffer object
-    GreetingServiceOuterClass.HelloResponse response = GreetingServiceOuterClass.HelloResponse.newBuilder()
-      .setGreeting("Hello there, " + request.getName())
-      .build();
+        // You must use a builder to construct a new Protobuffer object
+        GreetingServiceOuterClass.HelloResponse response = GreetingServiceOuterClass.HelloResponse.newBuilder()
+                .setGreeting("Hello there, " + request.getName())
+                .build();
 
-    // Use responseObserver to send a single response back
-    responseObserver.onNext(response);
+        System.exit(77);
+        
+        // Use responseObserver to send a single response back
+        responseObserver.onNext(response);
 
-    // When you are done, you must call onCompleted.
-    responseObserver.onCompleted();
-  }
+        // When you are done, you must call onCompleted.
+        responseObserver.onCompleted();
+    }
 }
