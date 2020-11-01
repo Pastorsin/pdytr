c13168232f965648d8fc6ed80a3fb6274ebc310b
Andrés Milla <andressmilla@gmail.com>
2020-11-01 02:55:05 -0300

    Added window arg to script of 4b

--- practica-3/ejercicios/scripts/script-4b.sh
+++ practica-3/ejercicios/scripts/script-4b.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-OUTPUT="../outputs/ejercicio-4b-test.csv"
-TIMES=20
+OUTPUT="../outputs/ejercicio-4b.txt"
+TIMES=10
 
+VENTANA=1
 ARCHIVO_SERVER="../ejercicio-4/database/server/experimento.txt"
 ARCHIVO_CLIENTE1="../ejercicio-4/database/client/experimento1.txt"
 ARCHIVO_CLIENTE2="../ejercicio-4/database/client/experimento2.txt"
 
 # Se limpia el output
-echo "Resultado" > $OUTPUT
+echo "Resultados" > $OUTPUT
 
 # Se posiciona en el directorio del ejercicio
-cd "/pdytr/practica-3/ejercicios/ejercicio-4"
+cd "../ejercicio-4"
 
 # Ejecuta el servidor
 mvn -DskipTests package exec:java -Dexec.mainClass=pdytr.example.grpc.App &
@@ -25,18 +26,18 @@ done
 
 # La opcion -q muestra solo excepciones y outputs del .java
 for (( i = 0; i < $TIMES; i++ )); do
-        mvn -DskipTests exec:java -Dexec.mainClass=pdytr.example.grpc.Client -Dexec.args="-escribir ${ARCHIVO_SERVER} ${ARCHIVO_CLIENTE1}" -q & 
-       	CLIENT_1_PID=$!
+        mvn -DskipTests exec:java -Dexec.mainClass=pdytr.example.grpc.Client -Dexec.args="-escribir ${ARCHIVO_SERVER} ${ARCHIVO_CLIENTE1} ${VENTANA}" -q & 
+            CLIENT_1_PID=$!
 
-       	mvn -DskipTests exec:java -Dexec.mainClass=pdytr.example.grpc.Client -Dexec.args="-escribir ${ARCHIVO_SERVER} ${ARCHIVO_CLIENTE2}" -q & 
-       	CLIENT_2_PID=$!
+            mvn -DskipTests exec:java -Dexec.mainClass=pdytr.example.grpc.Client -Dexec.args="-escribir ${ARCHIVO_SERVER} ${ARCHIVO_CLIENTE2} ${VENTANA}" -q & 
+            CLIENT_2_PID=$!
 
-       	wait $CLIENT_1_PID
-       	wait $CLIENT_2_PID
+            wait $CLIENT_1_PID
+            wait $CLIENT_2_PID
 
-       	echo "------------------" >> $OUTPUT
-       	echo "Ejecución $i: " >> $OUTPUT
-       	echo $(cat $ARCHIVO_SERVER) >> $OUTPUT
+            echo "------------------" >> $OUTPUT
+            echo "Ejecución $i: " >> $OUTPUT
+            echo $(cat $ARCHIVO_SERVER) >> $OUTPUT
 done
 
 kill -9 $SERVER_PID
