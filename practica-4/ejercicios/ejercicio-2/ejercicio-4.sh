03b14a8b7aca2b4a899b06e32bf672b78bf95fda
Ramiro Lopes Canadell <ramirolopescanadell@gmail.com>
2020-11-16 20:44:27 -0300

    script para el ejercicio 4

---
+++ practica-4/ejercicios/ejercicio-2/ejercicio-4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+javac -cp bin/jade.jar:src -d classes src/AgenteMovil.java
+
+java -cp bin/jade.jar jade.Boot -gui &
+SERVER_PID=$!
+
+until [ $(netstat -tulpn | grep "1099" | wc -l) -ge 1 ]; do
+      echo "INFO - Esperando que inicie el servidor"
+      sleep 1
+done
+
+java -cp bin/jade.jar:classes jade.Boot -gui -container -host localhost -agents mol:AgenteMovil &
+CLIENT_PID=$!
+
+sleep 1
+
+kill -9 $CLIENT_PID
+kill -9 $SERVER_PID
