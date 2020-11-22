14b0bd3050f711fe63afd8d94bc36a29ccafbbf2
Andrés Milla <andressmilla@gmail.com>
2020-11-22 03:23:29 -0300

    Refactor - Extract nested classes

--- practica-4/ejercicios/ejercicio-3/src/AgenteMovil.java
+++ practica-4/ejercicios/ejercicio-3/src/AgenteMovil.java
@@ -14,16 +14,15 @@ public class AgenteMovil extends Agent {
     private String pathDestino = "database/server.mp4";
     private String pathCopiaDestino = "database/server-copia.mp4";
 
-    private String containerDestino = "Container-1";
+    private String containerDestino = "Main-Container";
     private String containerOrigen;
 
-    private void moverAlContainer(String nombreContainer) {
-        ContainerID destino = new ContainerID(nombreContainer, null);
-        doMove(destino);
-    }
-
+    @Override
     public void setup() {
         try {
+            /* Control de argumentos */
+
+            /* Inicializacion de Comportamientos */
             containerOrigen = here().getName();
 
             SequentialBehaviour tareas = new SequentialBehaviour(this) {
@@ -35,17 +34,11 @@ public class AgenteMovil extends Agent {
             };
 
             TransferenciaBehaviour transferenciaCliente = new TransferenciaBehaviour(
-                containerOrigen,
-                containerDestino,
-                pathOrigen,
-                pathDestino
+                containerOrigen, containerDestino, pathOrigen, pathDestino
             );
 
             TransferenciaBehaviour transferenciaCopia = new TransferenciaBehaviour(
-                containerDestino,
-                containerOrigen,
-                pathCopiaDestino,
-                pathOrigen
+                containerDestino, containerOrigen, pathCopiaDestino, pathOrigen
             );
 
             tareas.addSubBehaviour(transferenciaCliente);
@@ -53,76 +46,14 @@ public class AgenteMovil extends Agent {
 
             addBehaviour(tareas);
 
-            moverAlContainer(containerDestino);
+            /* Se mueve al container destino para comenzar la lectura */
+            ContainerID destino = new ContainerID(containerDestino, null);
+            doMove(destino);
 
         } catch (Exception e) {
             System.out.println("No fue posible migrar el agente");
             e.printStackTrace();
+            doDelete();            
         }
     }
-
-    private class TransferenciaBehaviour extends Behaviour {
-        /*
-         * Transfiere un archivo que reside en el container destino hacia el
-         * container origen
-        */
-
-        private String origen;
-        private String destino;
-
-        private String pathOrigen;
-        private String pathDestino;
-
-        Transferencia transferencia = new Transferencia();
-
-        private boolean finalizada = false;
-
-        public TransferenciaBehaviour(String origen, String destino,
-                                      String pathOrigen, String pathDestino) {
-            super();
-
-            this.origen = origen;
-            this.destino = destino;
-
-            this.pathOrigen = pathOrigen;
-            this.pathDestino = pathDestino;
-        }
-
-        public void action() {
-            try {
-                String containerActual = here().getName();
-
-                if (containerActual.equals(origen)) {
-                    System.out.println("Escritura en " + containerActual);
-
-                    transferencia.escribir(pathOrigen);
-
-                    finalizada = transferencia.finalizada();
-
-                    if (!finalizada)
-                        moverAlContainer(destino);
-
-                } else {
-                    System.out.println("Lectura en " + containerActual);
-
-                    transferencia.leer(pathDestino);
-                    moverAlContainer(origen);
-                }
-
-            } catch (Exception e) {
-                System.err.println("ERROR - Escribir/Leer archivo");
-                e.printStackTrace();
-            }
-        }
-
-        public boolean done() {
-            return finalizada;
-        }
-
-        public int onEnd() {
-            System.out.println("Transferencia finalizada en " + pathOrigen);
-            return super.onEnd();
-        }
-    }
-
 }
---
+++ practica-4/ejercicios/ejercicio-3/src/TransferenciaBehaviour.java
@@ -0,0 +1,76 @@
+import jade.core.*;
+import jade.core.behaviours.*;
+
+public class TransferenciaBehaviour extends Behaviour {
+    /*
+     * Transfiere un archivo que reside en el container destino hacia el
+     * container origen
+    */
+    private String origen;
+    private String destino;
+
+    private String pathOrigen;
+    private String pathDestino;
+
+    Transferencia transferencia = new Transferencia();
+
+    private boolean finalizada = false;
+
+    public TransferenciaBehaviour(String origen, String destino,
+                                  String pathOrigen, String pathDestino) {
+        super();
+
+        this.origen = origen;
+        this.destino = destino;
+
+        this.pathOrigen = pathOrigen;
+        this.pathDestino = pathDestino;
+    }
+
+    private void moverAlContainer(String nombreContainer) {
+        ContainerID destino = new ContainerID(nombreContainer, null);
+        myAgent.doMove(destino);
+    }
+
+    private String containerActual() {
+        return myAgent.here().getName();
+    }
+
+    @Override
+    public void action() {
+        try {
+            
+            if (containerActual().equals(origen)) {
+                System.out.println("Escritura en " + containerActual());
+
+                transferencia.escribir(pathOrigen);
+
+                finalizada = transferencia.finalizada();
+
+                if (!finalizada)
+                    moverAlContainer(destino);
+
+            } else {
+                System.out.println("Lectura en " + containerActual());
+
+                transferencia.leer(pathDestino);
+                moverAlContainer(origen);
+            }
+        } catch (Exception e) {
+            System.err.println("ERROR - Escribir/Leer archivo");
+            e.printStackTrace();
+            myAgent.doDelete();
+        }
+    }
+
+    @Override
+    public boolean done() {
+        return finalizada;
+    }
+
+    @Override
+    public int onEnd() {
+        System.out.println("Transferencia finalizada en " + pathOrigen);
+        return super.onEnd();
+    }
+}
