f0aa22f1e48022136c9141cbcf256016bd156376
Andrés Milla <andressmilla@gmail.com>
2020-11-23 14:07:13 -0300

    Added arguments to Agent

--- practica-4/ejercicios/ejercicio-3/src/AgenteMovil.java
+++ practica-4/ejercicios/ejercicio-3/src/AgenteMovil.java
@@ -5,51 +5,63 @@ import jade.wrapper.*;
 
 /* TODO:
 - Establecer los parametros
-- Poner como opcion la escritura destructiva
-- Testear:
---> Escribir desde origen hacia origen
 */
 public class AgenteMovil extends Agent {
-    private String pathOrigen = "database/cliente.jpg";
-    private String pathDestino = "database/server.jpg";
-    private String pathCopiaDestino = "database/server-copia.jpg";
+    private String pathOrigen;
+    private String pathDestino;
+    private String pathCopiaDestino;
 
-    private String containerDestino = "Main-Container";
+    private String containerDestino;
     private String containerOrigen;
+
+    public void onDestroy() {
+        doDelete();
+        System.exit(0);
+    }
+
+    private void setupComportamientos() {
+        SequentialBehaviour tareas = new SequentialBehaviour(this) {
+            public int onEnd() {
+                System.out.println("Transferencias finalizadas.");
+                onDestroy();
+                return super.onEnd();
+            }
+        };
+
+        TransferenciaBehaviour transferenciaCliente = new TransferenciaBehaviour(
+            containerOrigen, containerDestino, pathOrigen, pathDestino
+        );
+
+        TransferenciaBehaviour transferenciaCopia = new TransferenciaBehaviour(
+            containerDestino, containerOrigen, pathCopiaDestino, pathOrigen
+        );
+
+        tareas.addSubBehaviour(transferenciaCliente);
+        tareas.addSubBehaviour(transferenciaCopia);
+
+        addBehaviour(tareas);
+    }
 
     @Override
     public void setup() {
-        try {
-            /* Control de argumentos */
+        /* Control de argumentos */
+        Object[] args = getArguments();
 
-            /* Inicializacion de Comportamientos */
-            containerOrigen = here().getName();
-
-            SequentialBehaviour tareas = new SequentialBehaviour(this) {
-                public int onEnd() {
-                    System.out.println("Transferencias finalizadas.");
-                    doDelete();
-                    return super.onEnd();
-                }
-            };
-
-            TransferenciaBehaviour transferenciaCliente = new TransferenciaBehaviour(
-                containerOrigen, containerDestino, pathOrigen, pathDestino
-            );
-
-            TransferenciaBehaviour transferenciaCopia = new TransferenciaBehaviour(
-                containerDestino, containerOrigen, pathCopiaDestino, pathOrigen
-            );
-
-            tareas.addSubBehaviour(transferenciaCliente);
-            tareas.addSubBehaviour(transferenciaCopia);
-
-            addBehaviour(tareas);
-
-        } catch (Exception e) {
-            System.out.println("No fue posible migrar el agente");
-            e.printStackTrace();
-            doDelete();            
+        if (args.length != 4) {
+            System.err.println("Argumentos invalidos, se necesitan 4 argumentos:");
+            System.err.println("<containerDestino> <pathOrigen>" +
+                               "<pathDestino> <pathCopiaDestino>");
+            System.exit(1);
         }
+
+        containerOrigen = here().getName();
+        containerDestino = args[0].toString();
+
+        pathOrigen = args[1].toString();
+        pathDestino = args[2].toString();
+        pathCopiaDestino = args[3].toString();
+
+        /* Inicializacion de Comportamientos */
+        setupComportamientos();
     }
 }
