73f52c91343cf0dc980a56428c8aa2600e80d6ea
Andrés Milla <andressmilla@gmail.com>
2020-11-24 22:14:34 -0300

    Added switch for read, write and read/write files

--- practica-4/ejercicios/ejercicio-3/src/AgenteMovil.java
+++ practica-4/ejercicios/ejercicio-3/src/AgenteMovil.java
@@ -11,54 +11,107 @@ public class AgenteMovil extends Agent {
     private String containerDestino;
     private String containerOrigen;
 
+    private final SequentialBehaviour tareas = new SequentialBehaviour(this) {
+        public int onEnd() {
+            System.out.println("Transferencias finalizadas.");
+            onDestroy();
+            return super.onEnd();
+        }
+    };
+
     public void onDestroy() {
         doDelete();
         System.exit(0);
     }
 
-    private void setupComportamientos() {
-        SequentialBehaviour tareas = new SequentialBehaviour(this) {
-            public int onEnd() {
-                System.out.println("Transferencias finalizadas.");
-                onDestroy();
-                return super.onEnd();
-            }
-        };
-
-        TransferenciaBehaviour transferenciaCliente = new TransferenciaBehaviour(
-            containerOrigen, containerDestino, pathOrigen, pathDestino
-        );
-
-        TransferenciaBehaviour transferenciaCopia = new TransferenciaBehaviour(
-            containerDestino, containerOrigen, pathCopiaDestino, pathOrigen
-        );
-
-        tareas.addSubBehaviour(transferenciaCliente);
-        tareas.addSubBehaviour(transferenciaCopia);
-
-        addBehaviour(tareas);
-    }
-
     @Override
-    public void setup() {
+    public void setup() {        
         /* Control de argumentos */
         Object[] args = getArguments();
 
-        if ((args == null) || (args.length != 4)) {
-            System.err.println("Argumentos invalidos, se necesitan 4 argumentos:");
-            System.err.println("<containerDestino> <pathOrigen>" +
-                               "<pathDestino> <pathCopiaDestino>");
+        if (args == null) {
+            System.err.println("Especifique alguna operacion: -r -w -rw");
             System.exit(1);
         }
 
+        String operacion = args[0].toString();
+
+        TransferenciaBehaviour leer = null;
+        TransferenciaBehaviour escribir = null;
+
         containerOrigen = here().getName();
-        containerDestino = args[0].toString();
 
-        pathOrigen = args[1].toString();
-        pathDestino = args[2].toString();
-        pathCopiaDestino = args[3].toString();
+        /* Identificacion de Operacion */
+        switch(operacion) {
+        case "-r":
+            if (args.length != 4) {
+                System.err.println("Argumentos invalidos, se necesitan 3 argumentos:");
+                System.err.println("-r <containerDestino> <pathOrigen> <pathDestino>");
+                System.exit(1);
+            }
 
-        /* Inicializacion de Comportamientos */
-        setupComportamientos();
+            containerDestino = args[1].toString();
+            pathOrigen = args[2].toString();
+            pathDestino = args[3].toString();
+
+            leer = new TransferenciaBehaviour(
+                containerOrigen, containerDestino, pathOrigen, pathDestino
+            );
+
+            tareas.addSubBehaviour(leer);
+
+            break;
+        case "-w":
+            if (args.length != 4) {
+                System.err.println("Argumentos invalidos, se necesitan 3 argumentos:");
+                System.err.println("-w <containerDestino> <pathOrigen> <pathDestino>");
+                System.exit(1);
+            }
+
+            containerDestino = args[1].toString();
+            pathOrigen = args[2].toString();
+            pathDestino = args[3].toString();
+
+            escribir = new TransferenciaBehaviour(
+                containerDestino, containerOrigen, pathDestino, pathOrigen
+            );
+
+            tareas.addSubBehaviour(escribir);
+
+            break;
+        case "-rw":
+            if ((args.length != 5)) {
+                System.err.println("Argumentos invalidos, se necesitan 4 argumentos:");
+                System.err.println("-rw <containerDestino> <pathOrigen>" +
+                                   "<pathDestino> <pathCopiaDestino>");
+                System.exit(1);
+            }
+
+            containerDestino = args[1].toString();
+            pathOrigen = args[2].toString();
+            pathDestino = args[3].toString();
+            pathCopiaDestino = args[4].toString();
+
+            leer = new TransferenciaBehaviour(
+                containerOrigen, containerDestino, pathOrigen, pathDestino
+            );
+
+            escribir = new TransferenciaBehaviour(
+                containerDestino, containerOrigen, pathCopiaDestino, pathOrigen
+            );
+
+            tareas.addSubBehaviour(leer);
+            tareas.addSubBehaviour(escribir);
+
+            break;
+        default:
+            System.err.println("Operacion invalida");
+            System.err.println("Operaciones disponibles: -r -w -rw");
+            System.exit(1);
+        }
+
+        /* Adicion de Comportamiento */
+        addBehaviour(tareas);
+
     }
 }
